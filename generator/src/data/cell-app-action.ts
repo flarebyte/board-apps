c9db09d296aecaa357f8c2593b5ca69ddd11de0c
Olivier Huin <olih@users.noreply.github.com>
2022-04-24 07:24:40 +0100

    Structure tables struct

--- generator/src/data/cell-app-action.ts
+++ generator/src/data/cell-app-action.ts
@@ -1,5 +1,5 @@
 import { AppAction } from "../model/application";
-import { cellTable, columnTable, horizonTable, rowTable } from "./cell-app-table";
+import { tables } from "./cell-app-table";
 import {
   addColumnMsg,
   addHorizonMsg,
@@ -18,71 +18,71 @@ export const addColumnAction: AppAction = {
   name: "add-column-action",
   kind: "append",
   inboundMessage: addColumnMsg,
-  table: columnTable,
+  table: tables.column,
 };
 
 export const updateColumnAction: AppAction = {
   name: "update-column-action",
   kind: "update",
   inboundMessage: updateColumnMsg,
-  table: columnTable,
+  table: tables.column,
 };
 export const deleteColumnAction: AppAction = {
   name: "delete-column-action",
   kind: "update",
   inboundMessage: deleteColumnMsg,
-  table: columnTable,
+  table: tables.column,
 };
 
 export const addRowAction: AppAction = {
   name: "add-row-action",
   kind: "append",
   inboundMessage: addRowMsg,
-  table: rowTable,
+  table: tables.row,
 };
 
 export const updateRowAction: AppAction = {
   name: "update-row-action",
   kind: "update",
   inboundMessage: updateRowMsg,
-  table: rowTable,
+  table: tables.row,
 };
 export const deleteRowAction: AppAction = {
   name: "delete-row-action",
   kind: "update",
   inboundMessage: deleteRowMsg,
-  table: rowTable,
+  table: tables.row,
 };
 
 export const addHorizonAction: AppAction = {
   name: "add-horizon-action",
   kind: "append",
   inboundMessage: addHorizonMsg,
-  table: horizonTable,
+  table: tables.horizon,
 };
 
 export const updateHorizonAction: AppAction = {
   name: "update-horizon-action",
   kind: "update",
   inboundMessage: updateHorizonMsg,
-  table: horizonTable,
+  table: tables.horizon,
 };
 export const deleteHorizonAction: AppAction = {
   name: "delete-horizon-action",
   kind: "update",
   inboundMessage: deleteHorizonMsg,
-  table: horizonTable,
+  table: tables.horizon,
 };
 
 export const updateCellAction: AppAction = {
   name: "update-cell-action",
   kind: "update",
   inboundMessage: updateCellMsg,
-  table: cellTable,
+  table: tables.cell,
 };
 export const deleteCellAction: AppAction = {
   name: "delete-cell-action",
   kind: "update",
   inboundMessage: deleteCellMsg,
-  table: cellTable,
+  table: tables.cell,
 };
--- generator/src/data/cell-app-table.ts
+++ generator/src/data/cell-app-table.ts
@@ -1,65 +1,72 @@
 import { AppColumn, AppTable } from "../model/application";
 
 const dimRow: AppColumn[] = [
-    {
-        name: "id",
-    },
-    {
-        name: "title",
-    },
-    {
-        name: "description",
-    },
-    {
-        name: "comment",
-    },
-    {
-        name: "homepage",
-    },
+  {
+    name: "id",
+  },
+  {
+    name: "title",
+  },
+  {
+    name: "description",
+  },
+  {
+    name: "comment",
+  },
+  {
+    name: "homepage",
+  },
 ];
-export const columnTable: AppTable = {
-    name: "column-table",
-    columns: [
-        ...dimRow,
-        {
-            name: "unit",
-        },
-        {
-            name: "media-type",
-        },
-    ],
+const columnTable: AppTable = {
+  name: "column-table",
+  columns: [
+    ...dimRow,
+    {
+      name: "unit",
+    },
+    {
+      name: "media-type",
+    },
+  ],
 };
-export const rowTable: AppTable = {
-    name: "row-table",
-    columns: dimRow,
+const rowTable: AppTable = {
+  name: "row-table",
+  columns: dimRow,
 };
-export const horizonTable: AppTable = {
-    name: "horizon-table",
-    columns: dimRow,
+const horizonTable: AppTable = {
+  name: "horizon-table",
+  columns: dimRow,
 };
-export const cellTable: AppTable = {
-    name: "cell-table",
-    columns: [
-        {
-            name: "column-id",
-        },
-        {
-            name: "row-id",
-        },
-        {
-            name: "horizon-id",
-        },
-        {
-            name: "text",
-        },
-        {
-            name: "unit",
-        },
-        {
-            name: "media-type",
-        },
-        {
-            name: "comment",
-        },
-    ],
+const cellTable: AppTable = {
+  name: "cell-table",
+  columns: [
+    {
+      name: "column-id",
+    },
+    {
+      name: "row-id",
+    },
+    {
+      name: "horizon-id",
+    },
+    {
+      name: "text",
+    },
+    {
+      name: "unit",
+    },
+    {
+      name: "media-type",
+    },
+    {
+      name: "comment",
+    },
+  ],
+};
+
+export const tables = {
+  column: columnTable,
+  row: rowTable,
+  horizon: horizonTable,
+  cell: cellTable,
 };
